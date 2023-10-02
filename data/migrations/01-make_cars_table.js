3f8519455d75a93fc917bbe34884feff84955fc4
dwilliams304 <d.williams0434@gmail.com>
2023-10-01 19:45:33 -0700

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,13 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments()
+    table.string('vin', 17).notNullable().unique();
+    table.string('make', 32).notNullable();
+    table.string('model', 32).notNullable();
+    table.numeric('mileage').unsigned().notNullable();
+    table.string('title', 16).defaultTo('clean');
+    table.string('transmission', 16).defaultTo('automatic');
+  })
 };
 
 exports.down = function (knex) {
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567891234',
+        make: 'kia',
+        model: 'stinger',
+        mileage: 10000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1234567891234',
+        make: 'volkswagen',
+        model: 'golf',
+        mileage: 25000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1234567891234',
+        make: 'honda',
+        model: 'civic',
+        mileage: 250000,
+        title: 'salvage',
+        transmission: 'automatic'
+    },
+]
+
+
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate();
+    await knex('cars').insert(cars);
+}
