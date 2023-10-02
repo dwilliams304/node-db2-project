f93392eb19e358c815379f5810628881eab30cb8
dwilliams304 <d.williams0434@gmail.com>
2023-10-01 20:09:42 -0700

    router wired

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,35 @@
 // DO YOUR MAGIC
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try{
+        res.json('test');
+    }catch(err){
+        next(err);
+    }
+    
+})
+router.get('/:id', async (req, res, next) => {
+    try{
+        res.json('test');
+    }catch(err){
+        next(err);
+    }
+    
+})
+router.post('/', async (req, res, next) => {
+    try{
+        res.json('test');
+    }catch(err){
+        next(err);
+    }
+    
+})
+
+
+
+
+
+module.exports = router;
